0aac18a9f0f8a5ae185d5444a0772bc86d257afb
Sudil Thennakoon <115799886+sudilvidmal@users.noreply.github.com>
2025-01-06 21:02:44 +0530

    update rout
    update rout

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SeatingComponent } from './seating/seating.component';
+import { UserloginComponent } from '../user/userlogin/userlogin.component';
+import { UsersignupComponent } from '../user/usersignup/usersignup.component';
+import { NgModule } from '@angular/core';
+
+import { AdminMovieComponent } from '../admin/admin-movie/admin-movie.component';
+import { AdminDashboardComponent } from '../admin/admin-dashboard/admin-dashboard.component';
+import { AdminDashboardMovieComponent } from '../admin/admin-dashboard-movie/admin-dashboard-movie.component';
+
+
+
+
+export const routes: Routes = [
+    { path: '', component: AdminDashboardComponent }, // Default route
+
+    { path: 'usersignup', component: UsersignupComponent },
+
+ 
+    { path: 'seating', component: SeatingComponent}
+
+];
